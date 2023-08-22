bb3ceb21757d0e327448514fc9553413d34ad5a5
MichelRibeiro48 <michel-amoral@hotmail.com>
2023-08-22 10:09:15 -0300

    First commit

---
+++ src/App.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("Something", () => {
+  expect(1 + 1).toBe(2);
+});
+
+test("Render Component", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("Vite + React")).toBeTruthy();
+});
